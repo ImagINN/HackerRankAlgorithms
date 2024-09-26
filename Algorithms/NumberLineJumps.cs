059f2005e53f7c14d9f0b3778207509538217373
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-09-26 23:43:52 +0300

    Add NumberLineJumps class for calculating if two kangaroos will meet on a number line

---
+++ Algorithms/NumberLineJumps.cs
@@ -0,0 +1,12 @@
+namespace HackerRankAlgorithms.Algorithms
+{
+    public class NumberLineJumps
+    {
+        public static string kangaroo(int x1, int v1, int x2, int v2)
+        {
+            string result = "YES";
+
+            return result;
+        }
+    }
+}
