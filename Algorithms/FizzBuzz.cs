595d732c3e1b605f55af9b15563a50c4d5be457d
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-09-30 21:25:43 +0300

    Refactor FizzBuzz class for improved efficiency and readability

---
+++ Algorithms/FizzBuzz.cs
@@ -0,0 +1,25 @@
+public class FizzBuzz
+{
+    public static void fizzBuzz(int n)
+    {
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
